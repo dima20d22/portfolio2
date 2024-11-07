22d9a4ca40ae487258d6f14c327d8a507e58046b
dima20d22 <113797583+dima20d22@users.noreply.github.com>
2024-11-07 15:13:01 +0000

    mobile done

---
+++ js/burger-window.js
@@ -0,0 +1,9 @@
+// document.querySelector('.burger').addEventListener('click', function () {
+// 	this.classList.toggle('active')
+// 	document.querySelector('.header__navi').classList.toggle('open')
+// })
+
+document.querySelector('.burger').addEventListener('click', function () {
+	this.classList.toggle('active')
+	document.querySelector('.header__navi').classList.toggle('opens')
+})
